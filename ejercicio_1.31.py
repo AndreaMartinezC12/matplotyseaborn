c0bbe82532fdc7ec9110af60b578996459f8e301
AndreaMartinezC <andrea12_94@hotmail.com>
2025-07-22 19:10:59 -0600

    Agregando tres graficas para mostrar el dataset de "flights" que muestra la cantidad de pasajeros de aviones al mes entre 1949 y 1960

--- ejercicio_1.31.py
+++ ejercicio_1.31.py
@@ -1,1 +1,14 @@
 #testing
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = sns.load_dataset("flights")
+
+print(sns.get_dataset_names())
+
+sns.barplot(x="month", y="passengers", data=df)
+#sns.boxplot(x="month", y="passengers", data=df)
+#sns.violinplot(x="month", y="passengers", data=df)
+plt.title("Cantidad de pasajeros en vuelos por mes entre los anios 1949 y 1960")
+
+plt.show()
